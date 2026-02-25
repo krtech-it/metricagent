f37b98172e5af4f81990d49d3c0131b0673fb028
Кирилл Петухов <test@mail.ru>
2026-02-25 15:13:41 +0300

    feat: increment 9 backup

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,16 +8,30 @@ import (
 )
 
 type Config struct {
-	Host           string
-	Port           int
-	ReportInterval int
-	PoolInterval   int
-	LogLevel       string
+	Host            string
+	Port            int
+	ReportInterval  int
+	PoolInterval    int
+	LogLevel        string
+	StoreInterval   int
+	FileStoragePath string
+	Restore         bool
 }
 
 func LoadConfig() (*Config, error) {
 	cfg := &Config{}
 	address := getEnv("ADDRESS", FlagServer.addr)
+	if storeInterval, err := strconv.Atoi(getEnv("STORE_INTERVAL", strconv.Itoa(FlagServer.storeInterval))); err == nil {
+		cfg.StoreInterval = storeInterval
+	} else {
+		cfg.StoreInterval = FlagServer.storeInterval
+	}
+	cfg.FileStoragePath = getEnv("FILE_STORAGE_PATH", FlagServer.fileStoragePath)
+	if restore, err := strconv.ParseBool(getEnv("RESTORE", strconv.FormatBool(FlagServer.restore))); err == nil {
+		cfg.Restore = restore
+	} else {
+		cfg.Restore = FlagServer.restore
+	}
 
 	args := strings.Split(address, ":")
 	if len(args) != 2 {
--- internal/config/flag.go
+++ internal/config/flag.go
@@ -5,13 +5,19 @@ import (
 )
 
 type SetServer struct {
-	addr string
+	addr            string
+	storeInterval   int
+	fileStoragePath string
+	restore         bool
 }
 
 var FlagServer = SetServer{}
 
 func ParseFlags() {
 	flag.StringVar(&FlagServer.addr, "a", "localhost:8080", "server listen address")
+	flag.IntVar(&FlagServer.storeInterval, "i", 300, "interval in seconds")
+	flag.StringVar(&FlagServer.fileStoragePath, "f", "storage.json", "file storage path")
+	flag.BoolVar(&FlagServer.restore, "r", false, "restore storage")
 	if !flag.Parsed() {
 		flag.Parse()
 	}
