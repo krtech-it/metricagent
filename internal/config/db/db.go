898a3366c50eb45af19ad8cf9e146611a00fb094
Кирилл Петухов <test@mail.ru>
2026-03-07 18:49:47 +0300

    iter 11 инициализация таблицы при старте

--- internal/config/db/db.go
+++ internal/config/db/db.go
@@ -13,5 +13,18 @@ func NewDB(dsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	InitSchema(db)
 	return db, nil
+}
+
+func InitSchema(db *sql.DB) error {
+	_, err := db.Exec(`
+		CREATE TABLE metrics (
+    id VARCHAR(50) PRIMARY KEY,
+    m_type VARCHAR(50) NOT NULL,
+    delta bigint default null,
+    value double precision default null
+)
+	`)
+	return err
 }
